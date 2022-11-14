efcce74212b1df37f489e97e481a7ea2e2c0178e
shamisashams <shamisaashams@gmail.com>
2022-11-14 14:55:45 +0400

    preloader added

--- resources/js/Layouts/Layout.js
+++ resources/js/Layouts/Layout.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 // import "./App.css";
 import { Link, usePage } from "@inertiajs/inertia-react";
-import { Inertia } from '@inertiajs/inertia'
+import { Inertia } from "@inertiajs/inertia";
 import "./index.css";
 import "aos/dist/aos.css";
 import Navbar from "../components/Navbar";
@@ -12,8 +12,23 @@ import setSeoData from "./SetSeoData";
 // import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Aos from "aos";
 import { Route } from "react-router-dom";
+import Preloader from "../components/Preloader";
 
 export default function Layout({ children, seo = null }) {
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, [5000]);
+    }, []);
+
+    Inertia.on("finish", () => {
+        setLoading(false);
+    });
+
+    window.addEventListener("load", () => {
+        setLoading(false);
+    });
 
     const { pathname } = usePage().props;
 
@@ -26,7 +41,6 @@ export default function Layout({ children, seo = null }) {
     //     )
     // }
 
-
     if (seo) {
         setSeoData(seo);
     }
@@ -37,18 +51,17 @@ export default function Layout({ children, seo = null }) {
     // console.log(usePage().props);
     const { currentLocale } = usePage().props;
 
-
     return (
-                <>
-                    {/*<Router>*/}
-                    {/*<Fragment>*/}
-                    {/* <CursorFollower /> */}
-                    <Navbar />
-                    {children}
-                    <Footer />
-                    {/*</Fragment>*/}
-                    {/*</Router>*/}
-                </>
-
+        <>
+            {/*<Router>*/}
+            {/*<Fragment>*/}
+            {/* <CursorFollower /> */}
+            <Navbar />
+            {children}
+            <Footer />
+            {/*</Fragment>*/}
+            {/*</Router>*/}
+            <Preloader loading={loading} />
+        </>
     );
 }
---
+++ resources/js/components/Preloader.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+
+const Preloader = ({ loading }) => {
+    const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+        setTimeout(() => {
+            if (counter < 100) {
+                setCounter(counter + 1);
+            }
+            if (counter === 100) {
+                loading = false;
+            }
+        }, 40);
+    }, [counter]);
+    console.log(counter);
+
+    return (
+        <div
+            className={`fixed w-screen h-screen z-50 left-0 top-0 bg-custom-dark ${
+                loading ? "block" : "hidden"
+            }`}
+        >
+            <div className="h-full flex flex-col justify-between items-center p-5 text-center">
+                <img className="" src="/assets/images/logo/1.png" alt="" />
+                <div className="my-10">
+                    <div className="loaderRing bold">{counter}%</div>
+                    <div className="bold text-2xl text-custom-yellow mt-10">
+                        Content is loading
+                    </div>
+                </div>
+                <div className="opacity-20 bold">
+                    <span>Construction works are done by</span>
+                    <a href="#">
+                        <img
+                            className="mx-auto mt-4 mb-2"
+                            src="/assets/images/logo/insite.png"
+                            alt=""
+                        />{" "}
+                        <span>www.insite.ge</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Preloader;
